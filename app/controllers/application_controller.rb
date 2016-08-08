7d4d035a8c92ba4e48d78a09896ca664825add1e
Joshua Ling <joshling1919@gmail.com>
2016-08-08 15:16:02 -0700

    Completed integration tests of authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    User.find_by_session_token(session[:session_token])
+    @current_user ||= User.find_by_session_token(session[:session_token])
   end
 end
---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "the signup process" do
+
+  scenario "has a new user page" do
+    visit new_user_url
+    expect(page).to have_content "New user"
+  end
+
+
+  feature "signing up a user" do
+    before(:each) do
+      visit new_user_url
+      fill_in 'username', :with => "testing_username"
+      fill_in "password", with: "password"
+      click_on "Create User"
+    end
+
+    scenario "redirects to goals page after signup" do
+      expect(page).to have_current_path(goals_path)
+    end
+
+    scenario "shows username on the homepage after signup" do
+      expect(page).to have_content "testing_username"
+    end
+
+  end
+
+end
+
+feature "logging in" do
+
+  feature "shows username on the homepage after login" do
+    before(:each) do
+      create(:user)
+      visit new_session_url
+      fill_in 'username', :with => "Bobert"
+      fill_in "password", with: "Slobert"
+      click_on "Log In"
+    end
+
+    scenario "redirects to goals page after login" do
+      expect(page).to have_current_path(goals_path)
+    end
+
+    scenario "shows username on the homepage after signup" do
+      expect(page).to have_content "Bobert"
+    end
+
+  end
+
+end
+
+feature "logging out" do
+
+  scenario "begins with a logged out state" do
+    visit new_session_url
+    expect(page).not_to have_content "Log Out"
+  end
+
+  scenario "doesn't show username on the homepage after logout" do
+    create(:user)
+    visit new_session_url
+    fill_in 'username', :with => "Bobert"
+    fill_in "password", with: "Slobert"
+    click_on "Log In"
+    click_on "Log Out"
+    expect(page).not_to have_content "testing_username"
+  end
+
+end
