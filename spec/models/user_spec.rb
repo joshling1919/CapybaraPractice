3a1261b1a153f78cf0fd9fe30bf5804b71d90d56
Joshua Ling <joshling1919@gmail.com>
2016-08-08 11:00:08 -0700

    created rsepc and migrations for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
+before(:each) do
+  bob = User.new
+end
+
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should validate length of password" do
+    should validate_length_of(:password).is_at_least(6)
+  end
+
+  it "should validate presence of usernamer" do
+    should validate_presence_of(:username)
+  end
+
+  it "should validate uniquness of username" do
+    should validate_uniqueness_of(:username)
+  end
+
+  it "should validate presence of password digest" do
+    should validate_presence_of(:password_digest)
+  end
+
+  it "should have a session token after intiailize" do
+    should validate_presence_of(:password_digest)
+  end
 end
